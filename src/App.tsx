d8b0e695c66ca1d898ccef718c7a92b9f3f5a997
unknown <lfcs18ts@gmail.com>
2022-07-25 12:10:17 -0300

    Primeiro commit projeto aula reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { count } from "console";
+import { useReducer } from "react";
 
-function App() {
+type reducerState = {
+  count: number;
+}
+
+type reducerAction = {
+  type: string;
+}
+
+const initialState = {count: 0};
+const reducer = (state: reducerState, action: reducerAction) => {
+  
+  switch(action.type){
+    case 'ADD':
+      return {...state, count: state.count + 1};
+    break;
+     case 'DEL':
+      if(state.count > 0){
+        return {...state, count: state.count - 1};
+      }
+    break;
+    case 'RESET':
+      return initialState;
+    break; 
+  }
+  
+  return state;
+}
+
+const App = () => {
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-5">
+      Contagem: {state.count}
+      <hr />
+      <button className="p-3" onClick={()=>dispatch({type:'ADD'})}>Adicionar</button>
+      <button className="p-3" onClick={()=>dispatch({type:'DEL'})}>Remover</button>
+      <button className="p-3" onClick={()=>dispatch({type:'RESET'})}>Resetar</button>
     </div>
   );
 }
