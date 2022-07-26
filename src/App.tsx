5c54d44f8b1eeb671f61473b291d395ce5d86568
unknown <lfcs18ts@gmail.com>
2022-07-25 22:52:56 -0300

    FInalizando com ordenar e deletar no reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,71 @@
+import { ChangeEvent, useState } from "react";
 import { useContagem } from "./reducers/contagem";
+import { usePeopleList } from "./reducers/people";
+
 
 const App = () => {
+
   const [contagem, contagemDispatch] = useContagem();
+  const [list, dispatchList] = usePeopleList();
+
+  const [nameInput, setNameInput] = useState('');
+
+  const handleAddButton = () => {
+    if (nameInput) {
+      dispatchList({
+        type: 'ADD',
+        payload: {
+          name: nameInput
+        }
+      });
+      setNameInput('');
+    }
+  }
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNameInput(e.target.value);
+  }
+
+  const deletePerson = (id: string) => {
+    dispatchList({
+      type: 'DEL',
+      payload: { id }
+    });
+  }
+
+  const handleOrderButton = () => {
+    dispatchList({
+      type: 'ORDER',
+      payload: {
+        name: undefined,
+        id: undefined
+      }
+    });
+  }
+
   return (
     <div className="p-5">
       Contagem: {contagem.count}
       <hr />
-      <button className="p-3" onClick={()=>contagemDispatch({type:'ADD'})}>Adicionar</button>
-      <button className="p-3" onClick={()=>contagemDispatch({type:'DEL'})}>Remover</button>
-      <button className="p-3" onClick={()=>contagemDispatch({type:'RESET'})}>Resetar</button>
+      <button className="p-3" onClick={() => contagemDispatch({ type: 'ADD' })}>Adicionar</button>
+      <button className="p-3" onClick={() => contagemDispatch({ type: 'DEL' })}>Remover</button>
+      <button className="p-3" onClick={() => contagemDispatch({ type: 'RESET' })}>Resetar</button>
+      <hr />
+
+      <p>Adicionar Pessoa</p>
+      <input type="text" value={nameInput} onChange={handleInputChange} />
+      <button onClick={handleAddButton}>Adicionar</button>
+      <hr />
+
+    <button onClick={handleOrderButton}>Ordenar</button>
+
+      <hr />
+      <p>Lista de Pessoas:</p>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item.name} <button onClick={() => deletePerson(item.id)}>Deletar</button></li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/reducers/people.ts
+++ src/reducers/people.ts
@@ -20,23 +20,30 @@ const reducer = (state: Person[], action: ActionType) => {
     switch(action.type){
         case 'ADD':
             if(action.payload.name){
-            state.push({
+                const newState = [...state];
+            newState.push({
                 id: uuidv4(),
                 name: action.payload.name
             });
+            return newState;
         }
         break;
         case 'DEL':
             if(action.payload.id){
-                state = state.filter(item => item.id !== action.payload?.id);
+                let newState = [...state];
+                newState = newState.filter(item => item.id !== action.payload?.id);
+                return newState;
             }
         break;
         case 'ORDER':
-            state = state.sort((a, b) => (a.name > b.name) ? 1 : -1);
+            let newState = [...state];
+            newState = newState.sort((a, b) => (a.name > b.name) ? 1 : -1);
+            return newState;
         break;
     }
+    return state;
 }
 
 export const usePeopleList = () => {
-
+    return useReducer(reducer, initialState);
 }
